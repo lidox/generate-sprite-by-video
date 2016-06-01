5aca468a01c3020491735e02cac1394a8a3d55f9
lidox <schaefa@mail.ru>
2016-06-01 19:44:33 +0200

    refactor

--- server.js
+++ server.js
@@ -3,32 +3,35 @@ var app = express();
 var path = require('path');
 var port = 443;
 
-//middleware configuration
-var batchFileWithPath = 'D:/projekte/generate-sprite-by-video/batch/convert.bat';
 
 // middleware
 app.use(express.static(__dirname + '/public'));
 
 // index.html
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/view/index.html'));
 });
+
 //C:/Users/schaefa/Desktop/convert.bat
 app.listen(port, function () {
-  console.log('App listening on port ' + port);
-
-	var spawn = require('child_process').spawn,
-	ls    = spawn('cmd.exe', ['/c', batchFileWithPath]);
-
-	ls.stdout.on('data', function (data) {
-	console.log('stdout: ' + data);
-	});
-
-	ls.stderr.on('data', function (data) {
-	console.log('stderr: ' + data);
-	});
-
-	ls.on('exit', function (code) {
-	console.log('child process exited with code ' + code);
-	});
+    console.log('App listening on port ' + port);
+    //executeBatch();
 });
+
+function executeBatch() {
+    var batchFileWithPath = 'batch/convert.bat';
+    var spawn = require('child_process').spawn,
+    ls = spawn('cmd.exe', ['/c', batchFileWithPath]);
+
+    ls.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+
+    ls.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
+    ls.on('exit', function (code) {
+        console.log('child process exited with code ' + code);
+    });
+}
