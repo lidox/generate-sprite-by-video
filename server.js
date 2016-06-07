60afecab608dfe0b7db572751ffa49575a617891
lidox <schaefa@mail.ru>
2016-06-07 16:37:09 +0200

    add create thumbs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var port = config.port;
 var VTTCreator = require('./core/vtt-creator.js');
 var im = require('./core/imagemagick-cli.js');
+var fs = require('fs');
 
 // middleware
 app.use(express.static(__dirname + '/public'));
@@ -16,29 +17,52 @@ app.get('/', function (req, res) {
 
 
 app.listen(port, function () {
-    console.log('App listening on port ' + port);
-	
-	var imageMagickDir = config.imagemagickDirectory;
-	var FFMPEG = imageMagickDir+'\\ffmpeg.exe\"';
-	
-	var VIDEO = 'C:\\Users\\schaefa\\Desktop\\Testing\\movies\\5ca7a557-4085-4a15-9e84-eea45927e9b1\\v_1_50.mp4';
-	var PATH2SPRITE = 'C:\\Users\\schaefa\\Desktop\\Testing\\thumbs';
+    console.log('App listening on port ' + port);	
+    
+    var PATH2SPRITE = 'C:\\Users\\russland\\Desktop\\Testing\\thumbs';
+    var PATH2VIDEO = 'C:\\Users\\russland\\Desktop\\Testing\\movies';
+    //var PATH2SPRITE = 'C:\\Users\\schaefa\\Desktop\\Testing\\thumbs';
+    //var PATH2VIDEO = 'C:\\Users\\schaefa\\Desktop\\Testing\\movies';
 	
 	var IMAGENAME = 'example-thumb';
 	var imgPerSecond = '10';
 	var widthPerImage = '400';
+    var VIDEO_NAME = 'v_1_50.mp4';
 	
-	var pathToFfmpeg = path.join(path.dirname(FFMPEG) , path.basename(FFMPEG));
-	var pathToVideo = path.join(path.dirname(VIDEO) , path.basename(VIDEO));
-	var imagesToUse = path.join(path.dirname(PATH2SPRITE) , path.basename(IMAGENAME +'%03d.png'));
-	//im.execCLI(pathToFfmpeg,  pathToVideo, imgPerSecond, widthPerImage, imagesToUse);
+
 	
 	//iterate over all folders inside movie folder:
-	var guidList = getDirectories(PATH2SPRITE);
-	console.log('getDirectories: '+guidList);
+	var guidList = getDirectories(PATH2VIDEO);
 	var index;
 	for (index = 0; index < guidList.length; ++index) {
-		console.log(guidList[index]);
+		//console.log('guiid:' + guidList[index]);
+        //var VIDEO = PATH2VIDEO + '\\5ca7a557-4085-4a15-9e84-eea45927e9b1\\v_1_50.mp4';
+        var VIDEO = path.join(path.join(PATH2VIDEO , guidList[index]), VIDEO_NAME); 
+        var PATH_TO_THUMBS = path.join(PATH2SPRITE, guidList[index]);
+        //console.log('PATH_TO_THUMBS: ' + PATH_TO_THUMBS);
+        if (!fs.existsSync(PATH_TO_THUMBS)){
+            fs.mkdirSync(PATH_TO_THUMBS);
+            console.log('create new folder: ' + PATH_TO_THUMBS);
+        }
+        
+        // check if video exists   
+        if (!fs.existsSync(VIDEO)){
+            console.log('video does not exist: ' + VIDEO);
+            // TODO: create log file
+        }
+        else {         
+            //console.log(VIDEO);
+            var imageMagickDir = config.imagemagickDirectory;
+            var FFMPEG = imageMagickDir+'\\ffmpeg.exe\"';
+            var pathToFfmpeg = path.join(path.dirname(FFMPEG) , path.basename(FFMPEG));
+            var pathToVideo = path.join(path.dirname(VIDEO) , path.basename(VIDEO));
+            var imagesToUse = path.join(PATH_TO_THUMBS , path.basename(IMAGENAME +'%03d.png'));
+            console.log('------------------');
+            console.log(index+ '. Versuch');
+            //console.log('pathToFfmpeg: ' + pathToFfmpeg + ' | pathToVideo:' + pathToVideo + ' | imgPerSecond:' + imgPerSecond+ ' | widthPerImage: ' + widthPerImage+ ' | imagesToUse:' + imagesToUse);
+            im.execCLI(pathToFfmpeg,  pathToVideo, imgPerSecond, widthPerImage, imagesToUse);
+        }
+
 	}
 
 	
@@ -54,8 +78,6 @@ app.listen(port, function () {
 	//var secsPerImgae = vtt.getSecondsPerImageByImgNr(50,25);
 	//console.log('seconds per image: ' + secsPerImgae);
 });
-
-var fs = require('fs');
 
 function getDirectories(srcpath) {
   return fs.readdirSync(srcpath).filter(function(file) {
